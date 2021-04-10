ba4b01833d7688a4b7c51171ca2d705ec65d658b
timurfromcanada <timurfromcanada@gmail.com>
2021-04-10 22:52:17 +0500

    Implemented HeatmapTask

--- HeatmapTask.cs
+++ HeatmapTask.cs
@@ -6,11 +6,32 @@ namespace Names
     {
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
-            return new HeatmapData(
-                "Пример карты интенсивностей",
-                new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-                new[] { "a", "b", "c", "d" }, 
-                new[] { "X", "Y", "Z" });
+            var days = new string[30];
+
+            for (int i = 0; i < 30; i++)
+            {
+                days[i] = (i + 2).ToString();
+            }
+
+            var month = new string[12];
+
+            for (int i = 0; i < 12; i++)
+            {
+                month[i] = (i + 1).ToString();
+            }
+
+            double[,] mounthDay = new double[30, 12];
+
+            foreach (var e in names)
+            {
+                if (e.BirthDate.Day != 1)
+                {
+                    mounthDay[e.BirthDate.Day - 2, e.BirthDate.Month - 1]++;
+                }
+            }
+                
+            return new HeatmapData("Пример карты интенсивностей", mounthDay, days, month);
         }
     }
 }
+
