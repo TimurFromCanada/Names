14b7446f8834f9deaf4f6af20f8f3044f3271c5c
timurfromcanada <timurfromcanada@gmail.com>
2021-04-10 22:49:29 +0500

    Implemented HistogramTask

--- HistogramTask.cs
+++ HistogramTask.cs
@@ -7,10 +7,26 @@ namespace Names
     {
         public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
         {
+            var days = new string[31];
+
+            for (var i = 0; i < days.Length; i++)
+            {
+                days[i] = (i + 1).ToString();
+            }
+
+            var countDates = new double[days.Length];
+
+            foreach (var e in names)
+            {
+                if (e.Name == name && e.BirthDate.Day != 1)
+                {
+                    countDates[e.BirthDate.Day - 1]++;
+                }
+            }
             return new HistogramData(
-                string.Format("Рождаемость людей с именем '{0}'", name), 
-                new string[0], 
-                new double[0]);
+                string.Format("Рождаемость людей с именем '{0}'", name),
+                days,
+                countDates);
         }
     }
 }
